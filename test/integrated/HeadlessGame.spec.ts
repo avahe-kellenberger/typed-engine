b0eabc66d10f09dbd0096e88f9a5b7a20d3a7872
Avahe Kellenberger <avahe@protonmail.ch>
2019-03-10 15:58:46 -0400

    Added test case for content's `update` method.

--- test/integrated/HeadlessGame.spec.ts
+++ test/integrated/HeadlessGame.spec.ts
@@ -1,10 +1,22 @@
 import { GameObject } from '../../src/entity/GameObject';
 import { HeadlessGame } from '../../src/HeadlessGame';
+import { Scene } from '../../src/scene/Scene';
 
 const game: HeadlessGame = new HeadlessGame();
 
-describe(`HeadlessGame`, () => {
-    it(`GameObject should be updated but not rendered`, () => {
+describe(`Content updating`, () => {
+    it(`Content should be updated only after game has started`, () => {
+        const scene: Scene = new Scene();
+        const spy = jest.spyOn(scene, 'update');
+        game.setContent(scene);
+
+        expect(spy).not.toHaveBeenCalled();
+        game.start();
+        expect(spy).toHaveBeenCalled();
+        game.stop();
+    });
+
+    it(`Renderable content should be updated but not rendered`, () => {
         const object: GameObject = new GameObject();
         const updateSpy: jest.SpyInstance = jest.spyOn(object, 'update');
         const renderSpy: jest.SpyInstance = jest.spyOn(object, 'render');
