5ea5f1911c2cb62d315a801772618d2d3df73917
Avahe Kellenbeger <avahe@protonmail.ch>
2019-06-09 16:57:25 -0400

    Fixed webpack options that have changed since the project was made.
    Added prettier config.

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  semi: false,
+  trailingComma: 'none',
+  singleQuote: true,
+  printWidth: 100,
+  tabWidth: 2,
+  singleQuote: true,
+  arrowParens: 'always',
+  endOfLine: 'lf'
+}
--- example/webpack.config.js
+++ example/webpack.config.js
@@ -1,21 +1,21 @@
-const path = require('path');
+const path = require('path')
 
 module.exports = {
-  entry: './src/ts/entry.ts',
+  entry: path.resolve(__dirname + '/src/ts/entry.ts'),
   resolve: {
     extensions: ['.ts', '.js']
   },
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, './dist'),
+    path: path.resolve(__dirname, './dist')
   },
   module: {
     rules: [
       {
         test: /\.ts$/,
         exclude: /node_modules/,
-        loader: 'awesome-typescript-loader',
+        loader: 'awesome-typescript-loader'
       }
     ]
   }
-};
+}
