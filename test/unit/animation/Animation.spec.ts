ba5f02bd45d24266a97ed63b00a0e5a9f6c431e9
Avahe Kellenberger <avahe@protonmail.ch>
2019-07-04 20:58:05 -0400

    Added a test for interpolated frame duration.

--- test/unit/animation/Animation.spec.ts
+++ test/unit/animation/Animation.spec.ts
@@ -6,14 +6,14 @@ describe(`getFrameAtTime`, () => {
   describe(`An animation with multiple frames`, () => {
     const frameDuration: number = 1.2
 
-    it(`Correctly interpolates location between two frames`, () => {
+    it(`Correctly interpolates frame properties between two frames`, () => {
       // Frame durations are the length of time before the next frame begins.
       // Interpolation occurs during an entire frame's duration until the next frame has started.
       const startLocation: Vector2D = new Vector2D(10, 10)
       const frame1 = new AnimationFrame(startLocation, frameDuration)
 
       const endLocation: Vector2D = new Vector2D(80, 80)
-      // The ending frame as no duration (last frame)
+      // The ending frame has no duration (last frame)
       const frame2 = new AnimationFrame(endLocation, 0)
 
       const animation: Animation = new Animation([frame1, frame2])
@@ -23,11 +23,15 @@ describe(`getFrameAtTime`, () => {
       const interpolatedFrame: AnimationFrame = animation.getInterpolatedFrameAtTime(
         interpolationTime
       )
+
       const distance: Vector2D = endLocation.subtract(startLocation)
       const expectedInterpolatedLocation: Vector2D = startLocation.add(
         distance.scale(interpolationRatio)
       )
       expect(interpolatedFrame.location).toEqual(expectedInterpolatedLocation)
+
+      const interpolatedTime: number = frame1.duration - frame1.duration * interpolationRatio
+      expect(interpolatedTime).toEqual(interpolatedFrame.duration)
     })
   })
 })
